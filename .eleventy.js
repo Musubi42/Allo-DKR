2332f139f0559c459daecf2ee65028992eda1faf
Musubi42 <raphael.dev@protonmail.com>
2023-07-14 16:34:57 +0200

    Base config eleventy

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+const tailwind = require('tailwindcss');
+const postCss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const postcssFilter = (cssCode, done) => {
+	// we call PostCSS here.
+	postCss([tailwind(require('./tailwind.config')), autoprefixer(), cssnano({ preset: 'default' })])
+		.process(cssCode, {
+			// path to our CSS file
+			from: './src/_includes/styles/tailwind.css'
+		})
+		.then(
+			(r) => done(null, r.css),
+			(e) => done(e, null)
+		);
+};
+
+module.exports = function (config) {
+  config.addWatchTarget("./src/_includes/styles/tailwind.css");
+  config.addNunjucksAsyncFilter("postcss", postcssFilter);
+  config.addPassthroughCopy("src/assets/fonts/");
+  config.addPassthroughCopy("src/assets/images/");
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    templateFormats: ["html", "md", "njk"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: ["./**/*.html"],
   theme: {
     extend: {
+      backgroundImage: {
+        hero: "url('/assets/images/hero.png')",
+        transition_star_primary: "url('/assets/images/transition_star_primary.png')",
+        transition_star_secondary: "url('/assets/images/transition_star_secondary.png')",
+        menu_restos: "url('/assets/images/menu_restos.png')",
+        menu_infos: "url('/assets/images/menu_infos.png')",
+      },
       colors: {
-        "primary": "#36593C",
+        "primary-light": "#36593C",
+        "primary-dark": "#36593C",
         "secondary": "#D6D9C1"
       },
       fontFamily: {
