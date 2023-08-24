0baa58237af0e929a0ba0d05e5e2a90e80c6827d
Musubi42 <thibaut.raphael@protonmail.com>
2023-08-24 16:41:44 +0200

    Add modal_landing_page

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,13 @@
-const tailwind = require('tailwindcss');
-const postCss = require('postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-
-const postcssFilter = (cssCode, done) => {
-	// we call PostCSS here.
-	postCss([tailwind(require('./tailwind.config')), autoprefixer(), cssnano({ preset: 'default' })])
-		.process(cssCode, {
-			// path to our CSS file
-			from: './src/_includes/styles/tailwind.css'
-		})
-		.then(
-			(r) => done(null, r.css),
-			(e) => done(e, null)
-		);
-};
+const modal_landing_page = require('./src/_includes/components/modal_landing_page')
 
 module.exports = function (config) {
-  config.addWatchTarget("./src/_includes/styles/tailwind.css");
-  config.addNunjucksAsyncFilter("postcss", postcssFilter);
+  // config.addWatchTarget("./src/_includes/styles/tailwind.css");
+  // config.addNunjucksAsyncFilter("postcss", postcssFilter);
   config.addPassthroughCopy("src/assets/fonts/");
   config.addPassthroughCopy("src/assets/images/");
-  config.addPassthroughCopy("src/scripts");
+  config.addPassthroughCopy("src/scripts/");
+
+  config.addShortcode("modal_landing_page", modal_landing_page)
   return {
     passthroughFileCopy: true,
     dir: {
